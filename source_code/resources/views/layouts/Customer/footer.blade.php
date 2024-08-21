171ce08935a72330cff96fd6e31a095cb8c3a24b
PutriGultom <putrigultom917@gmail.com>
2024-08-21 11:51:39 +0700

    update footer
    .

--- source_code/resources/views/layouts/Customer/footer.blade.php
+++ source_code/resources/views/layouts/Customer/footer.blade.php
@@ -2,7 +2,8 @@
     <footer class="footer spad">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-6">
+                <!-- Contact Info Section -->
+                <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="footer__about">
                         <h3 style="font-weight: bold; font-size: 24px;">Contact Info</h3><br>
                         <div class="item" style="display: flex; align-items: flex-start; margin-bottom: 10px;">
@@ -23,12 +24,14 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-5 col-md-6 col-sm-6 offset-lg-1">
+
+                <!-- Menu Links Section -->
+                <div class="col-lg-5 col-md-6 col-sm-12">
                     <div class="footer__widget">
                         <div class="row">
                             <div class="col-lg-6 col-md-6 col-sm-6">
                                 <h6>HELP & RESOURCES</h6>
-                                <ul style="text-decoration: underline">
+                                <ul style="text-decoration: underline;">
                                     <li><a href="/product">Find a product</a></li>
                                     <li><a href="/sign-up">Login member</a></li>
                                     <li><a href="/cart">Shopping Cart</a></li>
@@ -37,33 +40,38 @@
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-6">
                                 <h6>ABOUT</h6>
-                                <ul style="text-decoration: underline">
+                                <ul style="text-decoration: underline;">
                                     <li><a href="/company">About Us</a></li>
-                                    <li><a href="/company">Brand</a></li>
+                                    <li><a href="/company">Our Brand</a></li>
                                     <li><a href="/company">Contact Us</a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-12">
+
+                <!-- Logo Images Section -->
+                <div class="col-lg-3 col-md-12 d-flex justify-content-center">
                     <div class="footer__widget">
-                        <div class="footer__about__logo">
-                            <a href="./index.html"><img src="{{ asset('assets/images/logo.png') }}" alt="" style="width: 100%; height: 80px;"></a>
+                        <div class="footer__about__logo d-flex flex-column align-items-center">
+                            <a href="./index.html">
+                                <img src="{{ asset('assets/images/AGS-logo.png') }}" alt="" style="width: 100%; height: 100px; margin-bottom: 10px;">
+                            </a>
+                            <a href="./index.html">
+                                <img src="{{ asset('assets/images/logo.png') }}" alt="" style="width: 100%; height: 100px;">
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-12">
-                    <div class="footer__copyright">
-                        <div class="footer__copyright__text">
-                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                <div class="col-lg-12 d-flex justify-content-center">
+                    <div class="footer__copyright" style="padding: 15px 450px;">
+                        <div class="footer__copyright__text" style="font-size: 12px;">
                                 &copy;
                                 <script>
                                     document.write(new Date().getFullYear());
                                 </script> Created by PT Arkamaya Guna Saharsa
-                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                             </p>
                         </div>
                         <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""></div>
@@ -84,15 +92,9 @@
     <script src="{{ asset('ogani/js/mixitup.min.js') }}"></script>
     <script src="{{ asset('ogani/js/owl.carousel.min.js') }}"></script>
     <script src="{{ asset('ogani/js/main.js') }}"></script>
-    <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-    <!-- Bootstrap 5 JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-
-
     </body>
-
     </html>
